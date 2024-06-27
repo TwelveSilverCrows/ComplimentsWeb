1a00324385cd73689d07c0886c7cabed1ddf015d
Andrea J <109043962+TwelveSilverCrows@users.noreply.github.com>
2024-06-27 20:09:13 +0100

    Chooses a random element from an array and outputs to user in alert box on button click

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-function onButtonClick(){
-  alert("You are a great person!");
+const compliments = [
+  "Your smile is contagious!",
+  "You have a fantastic sense of humor.",
+  "You're incredibly creative.",
+  "I admire your work ethic.",
+  "You're a great listener.",
+  "You're always so helpful.",
+  "You have a bright future ahead of you.",
+  "Your positivity is inspiring.",
+  "You're a great problem-solver.",
+  "You have a wonderful way with words.",
+];
+function random(mn, mx) {
+  return Math.random() * (mx - mn) + mn;
+}
+function choosing() {
+  return compliments[(Math.floor(random(1, 11))) - 1]
+}
+function onButtonClick() {
+  alert(choosing());
 }
 const button = document.querySelector('button');
 button.addEventListener('click', onButtonClick);
